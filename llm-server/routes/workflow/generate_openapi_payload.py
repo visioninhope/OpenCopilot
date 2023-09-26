534c5c3226dc7335a88fd1ac43055128395f13bd
codebanesr <shanur.cse.nitap@gmail.com>
2023-09-26 23:02:00 +0300

    Swagger parser additions

--- llm-server/routes/workflow/generate_openapi_payload.py
+++ llm-server/routes/workflow/generate_openapi_payload.py
@@ -13,6 +13,7 @@ from typing import Dict, Any, Optional, Union, Tuple
 from routes.workflow.load_openapi_spec import load_openapi_spec
 
 from prance import ResolvingParser
+from prance import convert
 
 load_dotenv()
 
@@ -23,13 +24,16 @@ llm = get_llm()
 from typing import Dict, Any, Optional, Union, List
 
 # get path param, query param and json body schema for a given operation id
-def get_api_info_by_operation_id(data: Dict[str, Dict[str, dict]], target_operation_id: str) -> Dict[str, Union[str, dict, Optional[Dict[str, dict]]]]:
+from typing import Dict, Union, Optional, List
+
+def get_api_info_by_operation_id(data: Dict[str, Dict[str, dict]], target_operation_id: str) -> Dict[str, Union[str, dict, Optional[Dict[str, dict]], List[str]]]:
     api_info = {
         "endpoint": None,
         "method": None,
         "path_params": {},
         "query_params": {},
-        "body_schema": None
+        "body_schema": None,
+        "servers": []
     }
 
     for path, methods in data["paths"].items():
@@ -62,6 +66,11 @@ def get_api_info_by_operation_id(data: Dict[str, Dict[str, dict]], target_operat
                     request_body = details["requestBody"]
                     if "content" in request_body and "application/json" in request_body["content"]:
                         api_info["body_schema"] = request_body["content"]["application/json"]["schema"]
+
+                # Extract server URLs
+                servers = data.get("servers", [])
+                server_urls = [server["url"] for server in servers]
+                api_info["servers"] = server_urls
 
                 return api_info
 
@@ -96,5 +105,6 @@ def generate_openapi_payload(
         "method": api_info["method"],
         "path_params": path_params,
         "query_params": query_params,
-        "body_schema": body_schema
+        "body_schema": body_schema,
+        "servers": api_info["servers"]
     }
